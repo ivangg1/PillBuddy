60d926842d701ebb390b1ff9f37355101f75cb10
ivanG <ivangelhorn@gmail.com>
2025-11-20 09:12:46 +0100

    First Commit :p

---
+++ src/main/java/com/SOFTWAREgrupoX/pillbuddy/PillbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.SOFTWAREgrupoX.pillbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SOFTWAREgrupoX/pillbuddy/PillbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SOFTWAREgrupoX.pillbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
